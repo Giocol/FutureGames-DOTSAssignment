2402bd0baf59878ed6776388e9c8354938a81a2d
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2024-09-10 14:33:18 +0200

    feat: add mover component

---
+++ Assets/Scripts/Components/Mover.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Components {
+    public struct Mover : IComponentData {
+        public float movementSpeed;
+        public float3 movementDirection;
+    }
+
+}
---
+++ Assets/Scripts/Entities/MoverAuthoring.cs
@@ -0,0 +1,20 @@
+﻿using Components;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Entities
+{
+    public class MoverAuthoring : MonoBehaviour
+    {
+        public float movementSpeed;
+        public float3 movementDirection;
+
+        public class MoverBaker : Baker<MoverAuthoring> {
+            public override void Bake(MoverAuthoring authoring) {
+                Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new Mover { movementSpeed = authoring.movementSpeed, movementDirection = authoring.movementDirection });
+            }
+        }
+    }
+}
